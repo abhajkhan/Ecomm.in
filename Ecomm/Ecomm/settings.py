cdcc326e3cdfe80eeedb2b07a3c0eb7282d02837
Abhaj Khan <abhajkhan02@gmail.com>
2024-11-21 17:50:05 +0530

    product_list page logic&working done, need styling

--- Ecomm/Ecomm/settings.py
+++ Ecomm/Ecomm/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "store",
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ Ecomm/store/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self) :
+        return self.title
