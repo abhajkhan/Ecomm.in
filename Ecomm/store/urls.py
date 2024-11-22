2237887d826c3cb946dd05fdca2d552500487fcf
Abhaj Khan <abhajkhan02@gmail.com>
2024-11-22 23:24:43 +0530

    django-project: Ecomm.in completed with Home, Products, Contact Us, About Us pages

--- Ecomm/store/urls.py
+++ Ecomm/store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('about_us/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
+    path('category/<int:category_id>/', views.category_products, name='category_products'),
 
 ]
--- Ecomm/store/views.py
+++ Ecomm/store/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'store/home.html')
+    categories = Category.objects.prefetch_related('products')
+    return render(request, 'store/home.html', {'categories': categories})
+
+def category_products(request,category_id):
+    category = Category.objects.get(id=category_id)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/category_products.html', {'category': category, 'products': products})
 
 def product_list(request):
     products = Product.objects.all()
